08b461411fcce3a4336b44650fc44da85639864e
zts_UOM <tingshuoz@student.unimelb.edu.au>
2021-09-03 02:20:21 +1000

    Project overview page update
    1. GitHub  commit information
    2. Jira activities
    3. Confluence pages

--- src/ProjectPage/ProjectHomePage.js
+++ src/ProjectPage/ProjectHomePage.js
@@ -1,99 +1,188 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import uomHeader from "../header/uomheader.js";
 import { userActions } from "../_actions";
-import Table from "../_utils/Table";
-import Banner from "../_utils/Banner";
-import { Warining, InformationalNote } from "../_utils/Alert";
-import { alertConstants } from "../_constants";
+// import Table from "../_utils/Table";
+// import Banner from "../_utils/Banner";
+// import { Warining, InformationalNote } from "../_utils/Alert";
+// import { alertConstants } from "../_constants";
+import {
+  Table,
+  Space,
+  Collapse
+} from 'antd';
+import { github, jira } from "./temp";
+import {
+  H1,
+  ControlBar,
+  GitHubContent,
+  JiraContent,
+  ConfluenceContent,
+  Comment
+} from './style';
 
 class ProjectHomePage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [],
-
-      columns: [
+      show: "github",
+      page: "main",
+      gitColumns: [
         {
-          name: "Name",
-          selector: "name",
-          center: true,
-          sortable: true,
+          title: "Title",
+          dataIndex: "title",
+          key: "title",
         },
-
         {
-          name: "Profile",
-          selector: "picture",
-          center: true,
-          sortable: true,
-          cell: (row) => <img alt="Avatar" width="40" src={row.picture}></img>,
+          title: "Name",
+          dataIndex: "name",
+          key: "name",
+          align: "center"
         },
-
         {
-          name: "Student ID",
-          selector: "id",
-          center: true,
-          sortable: true,
+          title: "Time",
+          dataIndex: "time",
+          key: "time",
+          align: "center",
         },
-
         {
-          name: "Email Address",
-          selector: "email",
-          center: true,
-          sortable: true,
-        },
+          title: "",
+          dataIndex: "code",
+          key: "code",
+          render: (text, record) => (
+            <Space size="middle">
+              <a href="https://github.com/patanamon/COMP90082-SM1-2021-SP-Frontend/issues" style={{ textDecoration: "none", display: "inline-block" }}>
+                <img src="/icons/content.png" alt="content" />
+                {record.code}
+              </a>
+            </Space>
+          )
+        }
       ],
-    };
+      jiraColumns: [
+        {
+          title: "Comments",
+          dataIndex: "comments",
+          key: "comments",
+          ellipsis: true,
+          width: 500
+        },
+        {
+          title: "Work Log",
+          dataIndex: "worklog",
+          key: "worklog",
+          width: 200
+        },
+        {
+          title: "History",
+          dataIndex: "history",
+          key: "history",
+          width: 200
+        },
+        {
+          title: "Activity",
+          dataIndex: "activity",
+          key: "activity",
+          width: 300
+        },
+      ]
+    }
   }
-
-  componentDidMount() {
-    this.props.getTeamMemberList(this.props.currentTeamKey);
-  }
-
   render() {
+    const { Panel } = Collapse;
+    const P = ({ children }) => (
+      <span><img src="/icons/page.png" alt="page" /> &nbsp;&nbsp;&nbsp;{children}</span>
+    )
     return (
       <div className="uomcontent">
         {uomHeader("Project Overview")}
         <div role="main">
+          <H1>Project 1</H1>
           <div className="page-inner">
-            <Banner projName={this.props.currentTeamName} />
-            {!this.props.teamMemberList && (
-              <InformationalNote message={alertConstants.NO_DATA} />
-            )}
-            {this.props.teamMemberList &&
-              this.props.teamMemberList.length >= 30 && (
-                <Warining message={alertConstants.WRONG_CONFIG} />
-              )}
-            {this.props.teamMemberList &&
-              this.props.teamMemberList.length >= 30 && (
-              <Table
-                columns={this.state.columns}
-                data={this.props.teamMemberList.slice(0, 30)}
-                width="80vw"
-                height="50vh"
-              />
-            )}
-            {this.props.teamMemberList &&
-              this.props.teamMemberList.length < 30 && (
-                <Table
-                  columns={this.state.columns}
-                  data={this.props.teamMemberList}
-                  width="80vw"
-                  height="50vh"
-                />
-              )}
+            {this.state.page === "main"
+              ? <>
+                <ControlBar>
+                  <button>Members</button>
+                  <select name="content" style={{ width: "30%" }} onChange={(e) => { this.setState({ show: e.target.value }) }}>
+                    <option value="github">GitHub</option>
+                    <option value="jira">Jira</option>
+                    <option value="confluence">Confluence</option>
+                  </select>
+                  <button onClick={() => { this.setState({ page: "comment" }) }}>Comment</button>
+                </ControlBar>
+                {
+                  (() => {
+                    switch (this.state.show) {
+                      case "github":
+                        return <GitHubContent>
+                          <Table
+                            dataSource={github}
+                            columns={this.state.gitColumns}
+                            pagination={false}
+                          ></Table>
+                        </GitHubContent>
+                      case "jira":
+                        return <JiraContent>
+                          <Table
+                            dataSource={jira}
+                            columns={this.state.jiraColumns}
+                            pagination={false}
+                          ></Table>
+                        </JiraContent>
+                      case "confluence":
+                        return <ConfluenceContent>
+                          <Collapse ghost>
+                            <Panel header={<P>SP-Boxjelly Team</P>} >
+                              <Collapse ghost>
+                                <Panel header={<P>2.Design Concept</P>}>
+                                  <Collapse ghost>
+                                    <Panel header={<P>2.1 Goal</P>} />
+                                    <Panel header={<P>2.2 Business Case</P>}></Panel>
+                                    <Panel header={<P>2.3 Non-Functional Requirement</P>}></Panel>
+                                    <Panel header={<P>2.4 User Stories</P>}></Panel>
+                                    <Panel header={<P>2.5 Use-Case Model</P>}></Panel>
+                                    <Panel header={<P>2.6 Use-Case Specification</P>}></Panel>
+                                  </Collapse>
+                                </Panel>
+                                <Panel header={<P>3.Architecture</P>}>
+                                  <Collapse ghost>
+                                    <Panel header={<P>Architecture document</P>}></Panel>
+                                  </Collapse>
+                                </Panel>
+                                <Panel header={<P>4.Product Backlog</P>}>
+                                  <Collapse ghost>
+                                    <Panel header={<P>4.1. Sprint 1 - Backlog</P>}></Panel>
+                                    <Panel header={<P>4.2. Sprint 2 - Backlog</P>}></Panel>
+                                  </Collapse>
+                                </Panel>
+                              </Collapse>
+                            </Panel>
+                          </Collapse>
+                        </ConfluenceContent>
+                      default:
+                        return null;
+                    }
+                  })()
+                }
+              </>
+              : <Comment>
+                <div>Write your comments to Project 1:</div>
+                <textarea name="comment" cols="124" rows="15" style={{ display: "block" }}></textarea>
+                <button>Submit</button>
+              </Comment>
+            }
           </div>
         </div>
-      </div>
+      </div >
     );
   }
 }
 
 function mapState(state) {
+  console.log(state.user);
   return {
-    teamMemberList: state.user.teamMemberList,
     currentTeamKey: state.user.currentTeamKey,
-    currentTeamName: state.user.currentTeamName,
+    currentTeamName: state.user.currentTeamName
   };
 }
 const actionCreators = {
